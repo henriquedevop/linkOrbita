a0056f1715fb80e422bf525b738b302cf9848843
henriquedevop <wymuxgh@gmail.com>
2024-11-30 19:28:17 -0300

    feat: improve input component: add interface, props and submit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router";
 import { Input } from "../../components/input";
+import { FormEvent, useState } from "react";
 
 export function Login() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(e:FormEvent) {
+        e.preventDefault()
+        setEmail("")
+        setPassword("")
+
+        console.log({
+            email: email,
+            senha: password,
+        })
+    }
+
     return (
         <div className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
@@ -9,8 +25,20 @@ export function Login() {
                 <span className="bg-gradient-to-r from-indigo-700 to-violet-800 bg-clip-text text-transparent">Orbita</span></h1>
             </Link>
 
-            <form className="w-full max-w-xl flex flex-col px-2">
-                <Input/>
+            <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">
+                <Input 
+                type="email"
+                placeholder="Digite seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <Input 
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />  
 
                 <button 
                 className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
