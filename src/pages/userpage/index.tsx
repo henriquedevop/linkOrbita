3dece2dd2ba1e02006118183a8b5e0cecd7fd464
henriquedevop <wymuxgh@gmail.com>
2024-12-02 17:26:24 -0300

    feat: style userpage

--- src/pages/userpage/index.tsx
+++ src/pages/userpage/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router"
+import { useParams, Link } from "react-router"
 import { fireStore } from "../../services/firebaseConnection"
 import { collection, query, where, getDocs } from "firebase/firestore"
+import { FaLink } from "react-icons/fa"
 
 import { LinkProps } from "../admin"
 
@@ -48,22 +49,43 @@ export function UserPage() {
       },[username])
 
     return (
-        <main className="flex flex-col items-center min-h-screen p-4">
-            <h1 className="text-white">Links de {username}</h1>
-            {links.length > 0 ? (
-                links.map((item) => (
-                    <a
-                    href={item.url} target="_blank" 
-                    key={item.id}
-                    className="flex items-center justify-center w-11/12 max-w-xl rounded-md py-3 px-2 mb-2"
-                    style={{backgroundColor: item.bg, color: item.color}}
-                    >
-                    {item.name}
-                    </a>
-                ))
-            ) : (
-                <span>Adicione algo</span>
-            )}
+        <main className="flex flex-col items-center min-h-screen bg-gradient-to-b from-indigo-900 via-violet-800 to-indigo-900 p-4">
+            
+            <header className="w-full max-w-3xl text-center py-6">
+                <h1 className="text-4xl font-bold text-white">
+                    Links de <span className="bg-gradient-to-r from-violet-500 to-indigo-600 bg-clip-text text-transparent">{username}</span>
+                </h1>
+                <p className="text-zinc-300 mt-2">Clique em qualquer link para acessar!</p>
+            </header>
+            <section className="w-full max-w-3xl flex flex-col items-center">
+                {links.length > 0 ? (
+                    links.map((item) => (
+                        <a
+                        href={item.url} 
+                        target="_blank" 
+                        rel="noopenner noreferrer"
+                        key={item.id}
+                        className="w-11/12 flex items-center justify-center py-4 px-6 mb-4 bg-indigo-600 hover:bg-indigo-700 transition-all text-white font-semibold text-lg rounded-lg shadow-md"
+                        style={{backgroundColor: item.bg, color: item.color}}
+                        >
+                        {item.name}
+                        <FaLink className="ml-2 text-lg"/>
+                        </a>    
+                    ))
+                ) : (
+                    <div className="text-center mt-10">
+                        <p className="text-zinc-300 text-lg">Nenhum link adicionado ainda...</p>
+                        <p className="text-zinc-300 text-lg">Peça para <span className="text-white">{username} </span>
+                         adicionar algo interessante!</p>
+                    </div>
+                )}
+            </section>
+
+            <footer className="w-full max-w-3xl mt-8 text-center pt-4">
+                <p className="text-zinc text-sm">
+                Criado com ❤️ por <Link to="/" className="text-indigo-300 font-semibold">Link Órbita</Link>
+                </p>
+            </footer>
         </main>
     )
 }
